ac7ae9622a65329812202e4fd25ad2418e64bf32
BepisBoi424 <jasper.ward2005@gmail.com>
2022-12-16 11:19:18 -0700

    create elf objects in main

--- Elf.java
+++ Elf.java
@@ -1,9 +1,11 @@
 public class Elf 
 {
     public static int elfCount = 0;
-    public String name;
-    public double pay;
-    public Job job;
+    private String name;
+    private double pay;
+    private Job job;
+    private double totalMoney;
+
 
     public Elf(String name, double pay, Job job) 
     {
@@ -11,6 +13,10 @@ public class Elf
         this.pay = pay;
         this.job = job;
         
+    }
+
+    public int Pay() {
+        totalMoney += pay;
     }
     
     public double getMoney() {
--- Main.java
+++ Main.java
@@ -6,8 +6,16 @@ public class Main {
         Job sackPacker = new Job("Sack Packer", 18.19);
         Job manager = new Job("Manager", 55.28);
 
+        Elf scrumbo = new Elf("Scrumbo", 17.18, toyMaker);
+        Elf scrimblo = new Elf("Scrimblo", 17.18, toyMaker);
+        Elf dinglo = new Elf("Dinglo", 17.18, toyMaker);
+        Elf sogba = new Elf("Sogba", 15.16, giftWrapper);
+        Elf rugba = new Elf("Rugba", 15.16, toyMaker);
+        Elf brubga = new Elf("Brubga", 15.16, giftWrapper);
+        Elf brick = new Elf("The Brick", 18.12, sackPacker);
+        Elf thumb = new Elf("The Thumb", 18.12, sackPacker);
+        Elf blunt = new Elf("The Blunt", 7.25, sackPacker);
+        Elf ahole = new Elf("Ahole", 56.30, manager);
 
-        //Test 3
-        //I am about to throw this computer
     }
 }
